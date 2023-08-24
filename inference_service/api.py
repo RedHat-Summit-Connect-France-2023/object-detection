735efb30020d2ac042564f9cd79cbe4baf16fe0d
Adrien Legros <alegros@redhat.com>
2023-08-24 16:39:29 +0200

    s3 upload and url returned

--- inference_service/api.py
+++ inference_service/api.py
@@ -9,6 +9,10 @@ from dotenv import load_dotenv
 import ast
 
 import uuid
+import boto3
+import base64
+import io
+from PIL import Image
 
 load_dotenv()
 
@@ -22,6 +26,12 @@ CONF_THRESHOLD = float(os.getenv('CONF_THRESHOLD', 0.15))
 GRPC_HOST = os.getenv('GRPC_HOST', 'modelmesh-serving.object-detection')
 GRPC_PORT = os.getenv('GRPC_PORT', '8033')
 MODEL_NAME = os.getenv('MODEL_NAME', 'object-detection')
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "minio")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "minio123")
+AWS_S3_BUCKET = os.environ.get("AWS_S3_BUCKET", "images")
+AWS_S3_INTERNAL_ENDPOINT = os.environ.get("AWS_S3_INTERNAL_ENDPOINT", "")
+AWS_S3_ENDPOINT = os.environ.get("AWS_S3_ENDPOINT", "")
 
 class Box(BaseModel):
     xMax: float
@@ -43,8 +53,20 @@ class Detection(BaseModel):
 
 class Detections(BaseModel):
     detections: List[Detection]
+    image_url: str
 
 app = FastAPI()
+
+def push_to_bucket(safe_uid, random_uuid, img_data):
+    bucket_path = f"{safe_uid}/{random_uuid}.jpg"
+    is_success, buffer = cv2.imencode(".jpg", img_data)
+    io_buf = io.BytesIO(buffer)
+    s3_route = AWS_S3_INTERNAL_ENDPOINT if AWS_S3_INTERNAL_ENDPOINT else AWS_S3_ENDPOINT
+    s3_client = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, 
+                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                             endpoint_url=s3_route)
+    s3_client.upload_fileobj(io_buf, AWS_S3_BUCKET, bucket_path)
+    return bucket_path
 
 @app.post("/predictions", response_model=Detections)
 async def predictions(userId: Union[str, None] = None, file: UploadFile = File(...)):
@@ -53,10 +75,12 @@ async def predictions(userId: Union[str, None] = None, file: UploadFile = File(.
     img_data = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     if not userId:
         userId = "anonymous"
-    safe_uid = "".join([c for c in userId if c.isalpha() or c.isdigit() or c in ["@", "."]]).rstrip()
-    request_uuid = safe_uid + "_" + str(uuid.uuid4())
+    uid_b64 = base64.b64encode(str.encode(userId)).decode("utf-8")
+    random_uuid = str(uuid.uuid4())
+    request_uuid = uid_b64 + "_" + random_uuid
+    bucket_path = push_to_bucket(uid_b64, random_uuid, img_data)
     raw_detections = infer(img_data, CONF_THRESHOLD, IOU_THRESHOLD, request_uuid)
-    result = Detections(detections=[])
+    result = Detections(detections=[], image_url = f"{AWS_S3_ENDPOINT}/{AWS_S3_BUCKET}/{bucket_path}")
     for output in raw_detections:
         box = Box(xMin=output["xMin"], xMax=output["xMax"], yMin=output["yMin"], yMax=output["yMax"])
         detection = Detection(box=box, class_=output["class"],
